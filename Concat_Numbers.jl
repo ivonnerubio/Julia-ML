c13d470759a388a35311f833a0a89c7093d0103e
ivonnerubio <ivonnerubiouh@gmail.com>
2021-12-31 21:36:55 -0600

    concat strings

--- Concat_Numbers.jl
+++ Concat_Numbers.jl
@@ -1,2 +1,7 @@
 print("enter your number 1: ")
 number1=readline()
+print("enter your number 2: ")
+number2=readline()
+
+println("Here is your concat string $(number1 * number2)")
+println("Here is your added number: $(parse(Int64, number1) + parse(Int64, number2)) ")
---
+++ Lower_Upper_Case.jl
@@ -0,0 +1,4 @@
+char_d = 'd'
+char_d_index = convert(Int64, char_d)
+println("Lowercase letter: $(char_d_index)")
+println("Uppercase letter: $(char_d_index-32)")
